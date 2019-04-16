0e6d07f7666aaac2cad0068ab8bdcf1e6e8c17da
Axel <axelviskovic@gmail.com>
2019-04-16 11:39:42 +0200

    hamburger
    hamburger index

--- scripts/main.js
+++ scripts/main.js
@@ -3,26 +3,29 @@ const button = document.querySelector("#button")
 const buttonText = document.querySelector("#button a")
 const firstMenu = document.querySelector(".firstMenu")
 const teaser = document.querySelector(".teaser")
-
-
+const hamburger = document.querySelector("#hamburger")
+const blackHamburger = document.querySelector("#blackHamburger")
+const menuHamburger = document.querySelector(".menuHamburger")
+const logo = document.querySelector("logo")
 
 // PARAGRAPHE PRESENTATION
 
-if(arrowDown=true){
   triangle.addEventListener(
     "click",
     function(){
-      textTeaser.innerHTML="<p>Vous êtes un aventurier et vous devez tenter de survivre aux zombies dans un labyrinthe complexe. Pour cela, récupérez votre équipement, caché dans les différents recoins que vous devrez explorer. Dès le début de la partie, vous devez parcourir le labyrinthe afin de retrouver votre équipement. Mais ne trainez pas trop, un zombie affamé vous poursuit !<br> Lorsque vous avez retrouvé votre armure complète et votre arme, le zombie, malgré le fait qu'il n'ait pas de cerveau, va vous craindre, et il faudra le tuer pour passer au niveau supérieur. Mais il se peut qu'à ce moment la lumière s'éteigne, j'espère que vous n'avez pas peur du noir...<br><br>Jeu réalisé par l'equipe 18 composée de Julien Wagentrutz, Canan Yesilaltay, Nina Moilier, Axel Viskovic, et Nicolas Frappa.</p> "
+      textTeaser.innerHTML="<p>Vous êtes un aventurier et vous devez tenter de survivre aux zombies dans un labyrinthe complexe. Pour cela, récupérez votre équipement, caché dans les différents recoins que vous devrez explorer. Dès le début de la partie, vous devez parcourir le labyrinthe afin de retrouver votre équipement. Mais ne trainez pas trop, un zombie affamé vous poursuit !<br> Lorsque vous avez retrouvé votre armure complète et votre arme, vous pourrez enfin vous enfuir. Mais il se peut qu'à ce moment la lumière s'éteigne, j'espère que vous n'avez pas peur du noir...<br><br>Jeu réalisé par l'equipe 18 composée de Julien Wagentrutz, Canan Yesilaltay, Nina Moilier, Axel Viskovic, et Nicolas Frappa.</p> "
       triangle.style.display = "none"
 
     }
   )
-}
 
+// OUVERTURE DEBUT
 
 button.addEventListener(
   "click",
   function(){
+    hamburger.style.opacity="1"
+    hamburger.style.transition="all 2s"
     firstMenu.style.opacity="0"
     firstMenu.style.transition="all 2s"
 
@@ -38,14 +41,56 @@ button.addEventListener(
     teaser.style.display="block"
   }
 )
+
+
+// HAMBURGER
+
+hamburger.addEventListener(
+  "click",
+  function(){
+    menuHamburger.style.transform="translateX(200px)"
+    blackHamburger.style.opacity="1"
+  }
+)
+
+blackHamburger.addEventListener(
+  "click",
+  function(){
+    blackHamburger.style.opacity="0"
+    menuHamburger.style.transform="translateX(0px)"
+  }
+)
+
+
+
+
+
+
+
 /*
-margin : 30px 0px;
-width : 210px;
-height: 70px;
-background-color: red;
-border-radius: 50px;
-position : fixed;
-display: flex;
-justify-content: center;
-align-items: center;
+let nbImage=0
+
+if(nbImage<=2){
+  rightArrow.addEventListener(
+    "click",
+    function(){
+      console.log(nbImage)
+      sliderImg[nbImage].classList.remove("currentSlide")
+      sliderImg[nbImage+1].classList.add("currentSlide")
+      nbImage++
+    }
+  )
+}
+
+if(nbImage==2){
+  rightArrow.addEventListener(
+    "click",
+    function(){
+      window.alert("wsh")
+  sliderImg[2].classList.remove("currentSlide")
+  sliderImg[0].classList.add("currentSlide")
+  nbImage=0
+    }
+  )
+}
 */
