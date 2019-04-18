683c4cfce3453f66b59218c7a491747fbbc12930
Axel <axelviskovic@gmail.com>
2019-04-18 11:39:54 +0200

    start screen
    done

--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,6 @@
 const buttonStart = document.querySelector(".buttonStart")
 const musiqueJeu = document.querySelector("#musiqueJeu")
-
+const screenStart = document.querySelector(".screenStart")
 
 buttonStart.addEventListener(
   "click",
@@ -8,6 +8,9 @@ buttonStart.addEventListener(
     buttonStart.style.opacity="0"
     let game = new Game(document.querySelector('#game'));
     game.play()
-    musiqueJeu.play()
+    for(let i = 0; i<10; i++){
+      musiqueJeu.play()
+    }
+    screenStart.style.display="none"
   }
 )
